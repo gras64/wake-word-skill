e04fda77196d693de016c65b9cf15ad56708192b
gras64 <gars64@web.de>
2019-08-25 14:28:54 +0200

    initial

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
-from os.path import dirname, abspath, os, sys
+from os.path import dirname, expanduser, abspath, os, sys
 import time
 import uuid
 import subprocess
 import psutil as psutil
 import pyaudio
 import wget
+from shutil import rmtree
 from git import Repo
 from speech_recognition import Recognizer
 
@@ -18,10 +19,6 @@ from mycroft.util.time import now_local
 from mycroft.util.log import LOG, getLogger
 
 LOGGER = getLogger(__name__)
-#try:
-#from MycroftSkill.WakeWord.precise.precise.scripts.train import Trainer
-#except ImportError:
-    #self.log.info("precice not install")
 
 class WakeWord(MycroftSkill):
     def __init__(self):
@@ -32,6 +29,7 @@ class WakeWord(MycroftSkill):
         self.source_path = self.file_system.path
         self.piep = resolve_resource_file('snd/start_listening.wav')
 
+        self.settings["soundbackup"] = self.settings.get('soundbackup')
         self.settings["min_free_disk"] = 100  # min mb to leave free on disk
         self.settings["rate"] = 16000 # sample rate, hertz
         self.settings["channels"] = 1  # recording channels (1 = mono)
@@ -49,10 +47,10 @@ class WakeWord(MycroftSkill):
         if duration > 0:
             return subprocess.Popen(
                 ["arecord", "-r", str(rate), "-c", str(channels), "-d",
-                str(duration), "-f", str("S16_LE"), file_path])
+                str(duration), "-f", str(self.settings["formate"]), file_path])
         else:
             return subprocess.Popen(
-                ["arecord", "-r", str(rate), "-c", str(channels), "-f", str("S16_LE"), file_path])
+                ["arecord", "-r", str(rate), "-c", str(channels), "-f", str(self.settings["formate"]), file_path])
 
     def install_precice_source(self):
         if not os.path.isdir(self.file_system.path+"/precise"):
@@ -73,59 +71,66 @@ class WakeWord(MycroftSkill):
         free_mb = psutil.disk_usage('/')[2] / 1024 / 1024
         return free_mb - space > self.settings["min_free_disk"]
 
-    @intent_file_handler('word.wake.intent')
-    def word_wake_intent(self, message):
+    @intent_file_handler('wake.word.intent')
+    def wake_word_intent(self, message):
+        name = message.data.get("name")
+        name = name.replace(' ', '-')
+        from mycroft.configuration.config import Configuration
+        if name == Configuration.get()['listener']['wake_word']:
+            self.train_wake_word_intent(message)
+            return
+        else:
+            self.config(name, message)
+
+    @intent_file_handler('train.wake.word.intent')
+    def train_wake_word_intent(self, message):
         if message.data.get("name"):
             name = message.data.get("name")
+            name = name.replace(' ', '-')
+            if os.path.isdir(self.settings["file_path"]+"/"+name):
+                if self.ask_yesno("model.available",
+                                data={"name": name}) == "yes":
+                    rmtree(self.settings["file_path"]+"/"+name)
             self.speak_dialog("word.wake",
                                 data={"name": name})
             wait_while_speaking()
                 # Throw away any previous recording
-            try:
-                os.remove(self.settings["file_path"])
-            except:
-                pass
             time.sleep(4)
             i = 1
-            while i <= 4:
+            while i <= 12:
                 wait_while_speaking()
                 time.sleep(1)
                 if not self.record_process or wait_while_speaking():
                     play_wav(self.piep)
                     path = "wake-word/"+ self.lang + "-short/"
-                    if i == 2:
+                    if i => 9:
                         path = "test/wake-word/"+ self.lang + "-short/"
                     soundfile = name+ "-" + self.lang +"."+str(uuid.uuid1())+".wav"
                     self.start_recording(name,i,path,soundfile)
                     i = i + 1
-                    #if i == 5:
-                        #if not self.ask_yesno("is.all.ok") == "yes":
-                         #   i = 1
+                    if i == 13:
+                        if not self.ask_yesno("is.all.ok") == "yes":
+                           i = 1
             wait_while_speaking()
             self.speak_dialog("none.wake.word")
             time.sleep(4)
             i = 1
-            while i <= 4:
+            while i <= 12:
                 wait_while_speaking()
                 time.sleep(1)
                 if not self.record_process or wait_while_speaking():
                     play_wav(self.piep)
-                    path = "not-wake-words/"+ self.lang + "-short/"
-                    if i == 2:
-                        path = "test/not-wake-words/"+ self.lang + "-short/"
-                    soundfile = "not"+ name + "-"+ self.lang +"."+str(uuid.uuid1())+".wav"
+                    path = "not-wake-word/"+ self.lang + "-short/"
+                    if i => 9:
+                        path = "test/not-wake-word/"+ self.lang + "-short/"
+                    soundfile = "not"+name+"-"+ self.lang +"."+str(uuid.uuid1())+".wav"
                     self.start_recording(name,i,path,soundfile)
                     i = i + 1
-                    #if i == 5:
-                        #if not self.ask_yesno("is.all.ok") == "yes":
-                         #   i = 1
+                    if i == 13:
+                        if not self.ask_yesno("is.all.ok") == "yes":
+                            i = 1
             self.speak_dialog("start.calculating")
             self.calculating(name)
-
-
-
-
-
 
     def start_recording(self, name, i, path, soundfile):
         self.settings["duration"] = 3  # default recording duration
@@ -198,76 +203,104 @@ class WakeWord(MycroftSkill):
 
     def calculating(self, name):
         self.log.info("calculating")
-       # if not os.path.isdir(self.settings["file_path"]+"/"+name+"/not-wake-words/noises"):
+        ########### To do Unzip and processing Open Sound backup
+        #if self.settings["soundbackup"]:
+         #   if not os.path.isdir(self.settings["file_path"]+"/"+name+"/not-wake-word/noises"):
             # Create a ZipFile Object and load sample.zip in it
             #with libfile.extract_file(self.file_system.path+"/nonesounds.7z", 'r') as zipObj:
-             #   zipObj.extractall(self.settings["file_path"]+"/"+name+"/not-wake-words/noises")
-            #ZipFile.extractall(self.file_system.path+"/nonesounds.7z", self.settings["file_path"]+"/"+name+"/not-wake-words/noises")
+             #   zipObj.extractall(self.settings["file_path"]+"/"+name+"/not-wake-word/noises")
+            #ZipFile.extractall(self.file_system.path+"/nonesounds.7z", self.settings["file_path"]+"/"+name+"/not-wake-word/noises")
         #self.log.info("pid:"+ precise_calc.pid)
-        if self.settings.get("precise_calc_pid)") is None:
             #self.log.info("weiter"+precise_calc.pid)
-            #if os.isdir(self.settings["file_path"]+name+"/not-wake-words"):
-            self.precise_calc = subprocess.Popen([self.file_system.path+"/precise/.venv/bin/python "+
-                                        self.file_system.path+"/precise/precise/scripts/train.py "+
-                                        self.file_system.path+"/"+name+".net "+
-                                        self.settings["file_path"]+"/"+name+" -e "+ str(600)],
-                                        bufsize=-1, preexec_fn=os.setsid, shell=True)
-            self.settings["precise_calc_pid"] = self.precise_calc.pid
-            self.schedule_repeating_event(self.precise_check, None, 1,
+            #if os.isdir(self.settings["file_path"]+name+"/not-wake-word"):
+        self.precise_calc = subprocess.Popen([self.file_system.path+"/precise/.venv/bin/python "+
+                                    self.file_system.path+"/precise/precise/scripts/train.py "+
+                                    self.file_system.path+"/"+name+".net "+
+                                    self.settings["file_path"]+"/"+name+" -e "+ str(600)],
+                                    bufsize=-1, preexec_fn=os.setsid, shell=True)
+        self.settings["precise_calc_pid"] = self.precise_calc.pid
+        self.schedule_repeating_event(self.precise_check(name), None, 1,
                                           name='PreciseCalc')
-            return True
-        else:
-            return False
         return True
 
     def precise_check(self, name):
-        if not self.precise_calc:
+        if not self.precise_calc and not self.precise_convert:
             self.cancel_scheduled_event('PreciseCalc')
-            self.speak_dialog("end.calculating",
+            self.cancel_scheduled_event('PreciseConvert')
+            if not os.path.isfile(self.file_system.path+"/"+name+".test"):
+                self.precise_con(name)
+            else:
+                self.speak_dialog("end.calculating",
                             data={"name": name})
+    def precise_con(self, name):
+        self.precise_convert = subprocess.Popen([self.file_system.path+"/precise/.venv/bin/python "+
+                                    self.file_system.path+"/precise/precise/scripts/convert.py "+
+                                    self.file_system.path+"/"+name+".net "+
+                                    self.file_system.path+"/"],
+                                    bufsize=-1, preexec_fn=os.setsid, shell=True)
+        self.settings["precise_convert _pid"] = self.precise_convert.pid
+        self.schedule_repeating_event(self.precise_check(name), None, 1,
+                                          name='PreciseConvert')
+        return True
+
+    def select_precise_file(self, name, message):
+        if os.path.isfile(self.file_system.path+"/"+name+".pb"):
+            precise_file = self.file_system.path+"/"+name+".pb"
+            return precise_file
+        elif os.path.isfile("~/.mycroft/precise/"+name+".pb"):
+            precise_file = "~/.mycroft/precise/"+name+".pb"
+            return precise_file
+        else:
+            self.train_wake_word_intent(message)
+            return none
+            #precise_file = self.file_system.path+"/"+name+".pb"
+            #return precise_file
 
 
-   # def config(self, name):
-    #    from mycroft.configuration.config import (
-   #         LocalConf, USER_CONFIG, Configuration
-  #      )
-  #      module = message.data['ListenerType'].replace(' ', '')
-  #      module = module.replace('default', 'pocketsphinx')
-  #      name = module.replace('pocketsphinx', 'pocket sphinx')
 
-  #      if self.get_listener() == module:
- #           self.speak_dialog('listener.same', data={'listener': name})
-  #          return
+    def config(self, name, message):
+        from mycroft.configuration.config import (
+            LocalConf, USER_CONFIG, Configuration
+        )
+        module = "precise".replace(' ', '')
+        module = module.replace('default', 'pocketsphinx')
+        Name = module.replace('pocketsphinx', 'pocket sphinx')
 
-  #      wake_word = Configuration.get()['listener']['wake_word']
+        #if self.get_listener() == module:
+          #  self.speak_dialog('listener.same', data={'listener': name})
+         #   return
+        precise_file = self.select_precise_file(name, message)
+        if precise_file == "none":
+            self.log.info("precise file "+name+" not found")
+            return
+        else:
+            self.log.info("set precise file: "+precise_file)
+        wake_word = name
+        self.log.info("set precise WakeWord:"+name)
+        new_config = { "wake_word": name, 'hotwords': {wake_word:
+                    {'module': module, 'threshold': "1e-90", 'lang': self.lang,"local_model_file": precise_file}}
+        }
+        user_config = LocalConf(USER_CONFIG)
+        user_config.merge(new_config)
+        user_config.store()
 
- #       new_config = {
-#            'hotwords': {wake_word: {'module': module}}
- #       }
- #       user_config = LocalConf(USER_CONFIG)
- #       user_config.merge(new_config)
- #       user_config.store()
+        self.bus.emit(Message('configuration.updated'))
 
- #       self.bus.emit(Message('configuration.updated'))
+        if module == 'precise':
+            engine_folder = expanduser('~/.mycroft/precise/precise-engine')
+            if not os.path.isdir(engine_folder):
+                self.speak_dialog('download.started')
+                return
 
-#        if module == 'precise':
- #           engine_folder = expanduser('~/.mycroft/precise/precise-engine')
-  #          if not isdir(engine_folder):
-  #              self.speak_dialog('download.started')
-  #              return
-
-   #     self.speak_dialog('set.listener', data={'listener': name})
-
- #   def get_listener(self):
- #       """Raises ImportError or KeyError if not supported"""
-#        from mycroft.configuration.config import Configuration
- #       wake_word = Configuration.get()['listener']['wake_word']
- #       ww_config = Configuration.get()['hotwords'].get(wake_word, {})
- #       return ww_config.get('module', 'pocketsphinx')
-
+        self.speak_dialog('end.calculating', data={'name': name})
 
     @intent_file_handler('upload.intent')
     def upload_intent(self, message):
+        if message.data.get("name"):
+            name = message.data.get("name")
+            name = name.replace(' ', '-')
+        else:
+            name = Configuration.get()['listener']['wake_word']
         self.upload = True
 
 
